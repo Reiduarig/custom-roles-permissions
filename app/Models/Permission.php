8cd9ff78688f90e26e93f4e58efef250de04c2d9
Quique <quique@gmail.com>
2025-11-28 15:38:17 +0100

    completar modelos role y permiso

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,9 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Permission extends Model
 {
     /** @use HasFactory<\Database\Factories\PermissionFactory> */
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    /**
+     * The roles that belong to the permission.
+     * @return BelongsToMany<Role, $this>
+    */
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,9 +4,56 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
     /** @use HasFactory<\Database\Factories\RoleFactory> */
     use HasFactory;
+
+    // Define role constants SLUGS
+    public const SUPER_ADMIN = 'super-admin';
+    public const ADMIN = 'admin';
+    public const USER = 'user';
+
+    public const NIVEL_SUPER_ADMIN = 100;
+    public const NIVEL_ADMIN = 50;
+    public const NIVEL_USER = 10;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'nivel',
+    ];
+
+    /**
+     * The users that belong to the role.
+     * @return BelongsToMany<User, $this>
+    */
+
+    public function  users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     * The permissions that belong to the role.
+     * @return BelongsToMany<Permission, $this>
+    */
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function isHigherThan(Role $role): bool
+    {
+        return $this->nivel > $role->nivel;
+    }
 }
