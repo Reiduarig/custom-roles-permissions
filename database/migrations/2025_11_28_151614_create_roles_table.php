0cfb929f9ac8ac1c117fb9f831755621500ac34e
Quique <quique@gmail.com>
2025-11-28 15:27:51 +0100

    crear modelos role y permiso, factories y migraciones

---
+++ database/migrations/2025_11_28_151614_create_roles_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug')->unique()->comment('Identificador único del rol inmutable');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('nivel')->default(0)->comment('Jerarquía del rol, mayor número indica mayor nivel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+    }
+};
