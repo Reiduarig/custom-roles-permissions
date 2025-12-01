f7c4e97b6bca8055c6b7b8f1fdac490372c5adcc
Quique <quique@gmail.com>
2025-12-01 19:21:45 +0100

    policy user

---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, User $model): bool
+    {
+        if (! $user->hasPermission('view users')) {
+            return false;
+        }
+
+        // Un usuario no puede ver a otro usuario con un rol de mayor o igual nivel
+        return $this->canManageUsers($user, $model);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, User $model): bool
+    {
+        if (! $user->hasPermission('edit users')) {
+            return false;
+        }
+
+        // Un usuario no puede editar asi mismo
+        if ($user->id === $model->id) {
+            return false;
+        }
+
+        // Un usuario no puede editar a otro usuario con un rol de mayor o igual nivel
+        return $this->canManageUsers($user, $model);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, User $model): bool
+    {
+        if (! $user->hasPermission('delete users')) {
+            return false;
+        }
+
+        // Un usuario no puede eliminar asi mismo
+        if ($user->id === $model->id) {
+            return false;
+        }
+
+        // Un usuario no puede eliminar a otro usuario con un rol de mayor o igual nivel
+        return $this->canManageUsers($user, $model);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, User $model): bool
+    {
+        return false;
+    }
+
+    private function canManageUsers(User $user, User $model): bool
+    {
+        // Obtenemos el rol de mayor nivel del usuario autenticado
+        $userRole = $user->roles()->orderBy('level', 'desc')->first();
+            // Obtenemos el rol de mayor nivel del usuario objetivo
+        $modelRole = $model->roles()->orderBy('level', 'desc')->first();
+
+        if(!$userRole || !$modelRole) {
+            return false;
+        }
+
+        return $userRole->isHigherThan($modelRole);
+    }
+}
